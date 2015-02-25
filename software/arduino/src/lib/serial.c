cc5b6d4b2b70277cbac26b05eb9fcd5a42fa2d94
Thomas van Amsterdam <thomas1234@live.nl>
2015-02-25 20:59:05 +0100

    Serial functie
    serial functie SerialBegin gemaakt.

---
+++ software/arduino/src/lib/serial.c
@@ -0,0 +1,41 @@
+#define F_CPU 1000000
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <USART.h>
+#include <util/setbaud.h>
+
+#ifndef BAUD
+#define BAUD 9600
+#endif
+
+void serialBegin(){
+	UCSR0A |= (1 << U2X0);     // Double the USART Transmission Speed
+    UCSR0B |= (1 << TXEN0 | 1 << RXEN0); //Enable RX and TX
+    UCSR0C |= (1<<USBS0 | 1 << UCSZ01) | (1 << UCSZ00); //1 stopbit(USBS0) 8 databits(UCSZ00 & UCSZ01)
+}
+
+void serialEnd(){
+    UCSR0A &= ~(1 << U2X0);
+    UCSR0B &= ~(1 << TXEN0 | 1 << RXEN0);
+    UCSR0C &= ~(1<<USBS0 | 1 << UCSZ01) | (1 << UCSZ00);
+}
+
+
+char *serialRead();
+void serialWrite(char *);
+
+//www.appelsiini.net/2011/simple-usart-with-avr-libc
+//De code hieronder komt uit de arduino lib moet dit nog aanpassen
+/*
+void transmitByte(uint8_t data) {
+                                     // Wait for empty transmit buffer
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = data;                                            // send data
+}
+
+uint8_t receiveByte(void) {
+  loop_until_bit_is_set(UCSR0A, RXC0);       // Wait for incoming data
+  return UDR0;                                // return register value
+}
+*/
