6233cf1d7964fd7068726c0f1c0b9ddc75273053
Thomas van Amsterdam <thomas1234@live.nl>
2015-02-27 20:16:07 +0100

    serial update

--- software/arduino/src/lib/serial.c
+++ software/arduino/src/lib/serial.c
@@ -8,8 +8,19 @@
 #include <util/setbaud.h>
 #include "lib/serial.h"
 
+<<<<<<< HEAD
+#ifndef BAUD
+#define BAUD 9600
+#endif
+#ifndef BAUD_PRESCALLER
+#define BAUD_PRESCALLER (((F_CPU / (BAUD * 16UL))) - 1)
+#endif
+=======
+>>>>>>> origin/master
 
 void serialBegin(){
+    UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
+    UBRR0L = (uint8_t)(BAUD_PRESCALLER);
 	UCSR0A |= (1 << U2X0);     // Double the USART Transmission Speed
     UCSR0B |= (1 << TXEN0 | 1 << RXEN0); //Enable RX and TX
     UCSR0C |= (1<<USBS0 | 1 << UCSZ01) | (1 << UCSZ00); //1 stopbit(USBS0) 8 databits(UCSZ00 & UCSZ01)
@@ -22,10 +33,17 @@ void serialEnd(){
 }
 
 
-char *serialRead();
-void serialWrite(char *);
+char serialRead(void){
+    loop_until_bit_is_set(UCSR0A, RXC0); /* Wait until data exists. */
+    return UDR0;
+}
+void serialWrite(char c){
+    loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+    UDR0 = c;
+}
 
 //www.appelsiini.net/2011/simple-usart-with-avr-libc
+https://hekilledmywire.wordpress.com/2011/01/05/using-the-usartserial-tutorial-part-2/
 //De code hieronder komt uit de arduino lib moet dit nog aanpassen
 /*
 void transmitByte(uint8_t data) {
