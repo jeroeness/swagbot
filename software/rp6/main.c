ab739deeb54aa66818ffe71aa869f10b262372b7
Jucko13 <ricardoderoode@msn.com>
2015-03-04 20:54:39 +0100

    Main delay loop done
    50ms for the loop, if less is needed please tell me

--- software/rp6/main.c
+++ software/rp6/main.c
@@ -8,7 +8,8 @@
 #include "func_protos.h"
 
 struct sD sensorData;
-
+volatile uint8_t timer0OverflowCount = 0; //for timer overflow
+volatile uint8_t timer0Overflow = 0; //for timer overflow
 
 
 void testGlobalVariable(){
@@ -31,29 +32,62 @@ void testGlobalVariable(){
 	
 }
 
-
-
-
+void initTimerMain(){
+	//timer0: set prescaler of 1024
+	TCCR0 |= (1 << CS00) | (1<<CS02); 
+	
+	//timer0: init counter
+	TCNT0 = 0;
+	
+	//timer0: enable overflow interrupt
+	TIMSK |= (1 << TOIE0);
+	
+	//Set overflow variable to 0 (just to be sure)
+	timer0_overflow = 0;
+}
 
 
 int main(void) {
 	cli();
+	
 	initMotor();
+	initSensors();
+	initTimerMain();
+	
 	sei();
 	
-	initSensors();
-	
-	//setLed(2,1);
-	
-	//DDRB |= 1<<PB3; //green led as output
-
-	stop();
+	stop(); //stop the motors
 	
 	while(1) {
-		readSensors();
+		if(timer0Overflow == 1){ //timed script sequence of 50ms
+			timer0Overflow = 0;
+			
+			readSensors();
+		}
+		
 		testGlobalVariable();
 	}
 	
 	return 0;
 }
 
+
+
+
+ISR(TIMER0_OVF_vect)
+{
+	timer0OverflowCount++;
+	if(timer0OverflowCount == 1){
+		TCNT0 = 122; //count from 122 to 256(overflow) to create pulse of 50ms
+	}else{
+		timer0Overflow = 1;
+		timer0OverflowCount = 0;
+	}
+	
+	//formula to check the number of milliseconds:
+	//delay_in_seconds / (1/F_CPU*prescaler) - 1 = (0.050 / (1/8000000*1024) = 389.625
+	//390.625 - 256 = 134,625 //need to overflow 1 and almost another half time
+	// 256 - 133,625 = start value of timer on second interrupt = 122.375
+	
+	//complete formula: 256-(((0.050/(1/8000000*1024))-1)-256)
+}
