f3a7ca79d5352849b364d235a15e812d3b3f108b
stonethehuman <stonethehuman@gmail.com>
2015-03-05 14:19:18 +0100

    moved motor functions to arduino

--- software/arduino/src/modules/motor.c
+++ software/arduino/src/modules/motor.c
@@ -1,2 +1,79 @@
-#include "motor.h"
 
+#include <stdio.h>
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "motor.h"
+#include "sensor.h"
+
+void initMotor(){
+	TCNT1=0x00;
+	TCCR1A = (0 << WGM10) | (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1);
+	TCCR1B = (1 << WGM13) | (0 << WGM12) | (1 << CS10);
+	
+	ICR1=0x00FF;
+	OCR1A=0x0050;
+	OCR1B=0x0050;
+	
+	TIMSK &= ~(1<< TICIE1) &~(1<< OCIE1A) &~(1<< OCIE1B) &~(1<< TOIE1);
+	
+	DDRC |= (1 << PINC2) | (1 << PINC3);
+	PORTC |= (1 << PINC2);
+	PORTC &= ~(1 << PINC3);
+}
+
+ISR(TIMER1_COMPA_vect){
+}
+
+ISR(TIMER1_COMPB_vect){
+}
+
+void motorTest() {
+	drive(100, 50, -1);
+	_delay_ms(3000);
+	drive(100, -50, -1);
+	_delay_ms(3000);
+	stop();
+	_delay_ms(3000);
+	turn(100);
+	_delay_ms(3000);
+	turn(50);
+	_delay_ms(3000);
+}
+
+
+// negative deflection = left; positive deflection = right
+void drive(int8_t speed, int8_t deflection, int8_t direction) {
+	int speedL = speed;
+	int speedR = speed;
+  
+	if (deflection > 0) {
+	   speedR -= deflection; 
+	} else {
+	   speedL += deflection; 
+	}
+	moveMotors(direction * speedL, direction * speedR);
+}
+
+void turn(int8_t speed) {
+	moveMotors(speed, -speed);
+}
+
+void stop() {
+	moveMotors(0, 0);
+}
+
+//dirL, dirR direction of left and right motor. 1=forward, -1=backward
+//speedL, speedR movement speed of left and right motor. 0 <= speed <= 200
+int moveMotors(int8_t speedL, int8_t speedR){
+	if(speedR < -100 || speedR > 100)
+	    return 0;
+	if(speedL < -100 || speedL > 100)
+	    return 0;
+
+	sensorData.motorLeft = speedL;
+	sensorData.motorRight = speedR;
+	
+	return 1;
+}
--- software/arduino/src/modules/motor.h
+++ software/arduino/src/modules/motor.h
@@ -1,5 +1,43 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+/*---------------- MOTOR functions ----------------*/
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+
+/**	Makes the RP6 drive and deflect from it's current path if necessary.
+ * 
+ *	@param speed		the speed at which the RP6 will drive 
+ * 	@param deflection	the amount the RP6 should deflect when driving (deflect < 0: The RP6 will deflect to the left, deflect > 0: The RP6 will deflect to the right, deflect == 0: The RP6 won't deflect) 
+ */
+void drive(int8_t speed, int8_t bend, int8_t direction);
+
+/**	Makes the RP6 turn around it's own axis.
+ * 
+ *	@param speed		the speed at which the RP6 will turn 
+ */
+void turn(int8_t speed);
+
+/**	Makes both of the RP6 motors stop with everything they're doing.
+ */
+void stop();
+
+/**	This function initializes everything that is necessary for the motor functions to work.
+ * 
+ *	@param speed		the speed at which the RP6 will turn 
+ */
+void initMotor();
+
+/**	Set the motor variables to be sent to the RP6 by I2C
+ *
+ *	@param speedL 		the speed of the left motor (min: -100, max: 100)  negative for backwards, positive for forwards.
+ *	@param speedR 		the speed of the right motor (min: -100, max: 100)  negative for backwards, positive for forwards.
+ * 	@return 			1 if successfull, 0 if wrong speed
+ */
+int moveMotors(int8_t speedL, int8_t speedR);
+
+/**	A simple function to test the motor functions.
+ */
+void motorTest();
 
 #endif // MOTOR_H
