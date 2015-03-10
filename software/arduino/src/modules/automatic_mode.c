ee88d2f8a6ba33ba6272f04ed84d70384a298634
CvdH <casperv.d.hout@hotmail.com>
2015-03-10 17:02:39 +0100

    faal 2

--- software/arduino/src/modules/automatic_mode.c
+++ software/arduino/src/modules/automatic_mode.c
@@ -6,7 +6,7 @@
 #include "automatic_mode.h"
 #include "motor.h"
 #include "sensor.h"
-#include "mode_manager.h"
+//#include "mode_manager.h"
 
 #define TURN_MARGIN 5
 #define MOVE_MARGIN 5
@@ -103,10 +103,9 @@ ISR(TIMER0_OVF_vect)
 }
 
 void checkCrash() {
-	if (/*sensorData.ultrasonic < 10 ||*/ sensorData.bumperLeft || sensorData.bumperRight) {
+	if (sensorData.ultrasonic < 10 || sensorData.bumperLeft || sensorData.bumperRight) {
 		stop();
-		SteeringMode s = manual;
-		setSteeringMode(s);
+		//setSteeringMode(SteeringMode.manual);
 		resetAutomaticMode();
 	}
 }
