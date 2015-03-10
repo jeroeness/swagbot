de8ff6be20dc47862ac3e5f34906e71b700ba002
ger-ald <gerald.elzing@gmail.com>
2015-03-10 10:52:45 +0100

    i2c added

--- software/rp6/func_protos.h
+++ software/rp6/func_protos.h
@@ -1,3 +1,12 @@
+//func_protos.h
+#define F_CPU 8000000 //Base: 8.00MHz - DO NOT CHANGE! // CPU works @ 8.0Mhz;
+
+#include <stdio.h>
+#include <util/delay.h>
+#include <avr/io.h> // I/O Port definitions
+#include <avr/interrupt.h> // Interrupt macros (e.g. cli(), sei())
+
+
 #ifndef MOTORHEADER
 #define MOTORHEADER
 
@@ -44,21 +53,25 @@ struct ID{
 	uint8_t ledStatus; //8 all leds in one variable
 };
 
-extern struct ID instructionData;
-extern struct SD sensorData;
+struct SD sensorStruct;
+struct ID instructionstruct;
 
 union USD {
-	struct SD s;
-	uint8_t sendData[sizeof(sensorData)];
+	struct SD sensorStruct;
+	uint8_t sensorArray[sizeof(sensorStruct)];
 };
 
 union UID {
-	struct ID i;
-	uint8_t receiveData[sizeof(instructionData)];
+	struct ID instructionstruct;
+	uint8_t instructionArray[sizeof(instructionstruct)];
 };
 
+union UID instructionData;
+union USD sensorData;
 
 void initSensors();
+
+void parseInstruction(void);
 
 void setLed(uint8_t uLed, uint8_t uOn);
 
@@ -67,5 +80,22 @@ void readBumperR();
 void readCompass();
 void readSensors();
 
+
+#endif
+
+#ifndef I2CLIBHEADER
+#define I2CLIBHEADER
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+/*----------------- I2C functions -----------------*/
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+
+/* I2C clock in Hz */
+#ifndef I2C_CLOCK
+#define I2C_CLOCK 100000UL
+#endif
+
+void i2c_init(uint8_t slaveaddress);
+void i2c_write_cmd(uint8_t);
+void i2c_read_sensors(uint8_t);
 
 #endif
