17b71590398681899d30254eb4cae4d5e831a849
gerald <gerald.elzing@gmail.com>
2015-03-24 22:29:41 +0100

    this should work ...
    put actual code in the files

--- software/arduino/src/modules/ultrasonic_sensor.h
+++ software/arduino/src/modules/ultrasonic_sensor.h
@@ -1,1 +1,11 @@
 // ultrasonic_sensor.h
+
+#ifndef ULTRASONIC_SENSOR_H
+#define ULTRASONIC_SENSOR_H
+
+//#include "../globalincsanddefs.h"
+
+void initUltrasonicSensor(void);
+void UltrasonicSensorRead(void);
+
+#endif // ULTRASONIC_SENSOR_H
