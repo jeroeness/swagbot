ec9e142448353a14e2d667554ce24e0cbb6ebd40
Jeroen <jeroenesseveld@gmail.com>
2015-03-26 10:57:47 +0100

    fixed move motors issue thanks to Ricardo pointing out my mistake.

--- software/arduino/src/modules/manual_mode.c
+++ software/arduino/src/modules/manual_mode.c
@@ -10,23 +10,21 @@ int8_t direction;
 uint8_t collision;
 
 void inputRight() {
-	direction = 120; //jeroen this is not right. in movemotors it says this value cant be over 100 and under -100.
-	//if you only press the d then it will be 120 (over the 100). Please fix this.
+	direction = 120;
 }
 
 void inputLeft() {
-	direction = -120; //jeroen this is not right. in movemotors it says this value cant be over 100 and under -100.
-	//if you only press the a then it will be -120 (under the -100). Please fix this.
+	direction = -120;
 }
 
 void inputBackward() {
 	forward = -90;
-	collision = 0;
 }
 
 void inputForward() {
-    forward = 90;
-    collision = 0;
+	if (!collision) {
+		forward = 90;
+	}
 }
 
 
@@ -60,16 +58,17 @@ void alterCourse () {
 	} else if (forward == -90) {
 		moveMotors(max(forward + direction, -100), max(forward - direction, -100));
 	} else  {
-		moveMotors(forward + direction, forward - direction);
+		moveMotors(max(min(direction, 100), -100), max(min(-direction, 100), -100));
 	}
 }
 
 void manualModecheckCollision () {
 	uint8_t c = sensorData.sensorStruct.bumperRight == 1 || sensorData.sensorStruct.bumperLeft == 1;
-	if (c != collision && c) {
-		//TODO when we know for sure the bumperdata is correct we can uncomment this line
-        //forward = min(0, forward);
-		alterCourse();
+	if (c != collision) {
+		if (c) {
+			forward = min(0, forward);
+			alterCourse();
+		}
 		collision = c;
 	}
 
